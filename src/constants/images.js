c88965d5e02be3ed376aa7e8088677e4d68b38ff
waltodd <waltodd@hotmail.com>
2022-06-13 10:13:08 +0100

    Testimonial

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,4 +1,6 @@
 import artboard from '../assets/Artboard.svg';
+import DoubleQuotes from '../assets/Big-double-quote.svg';
 export default {
-    artboard
+    artboard,
+    DoubleQuotes
 }
--- src/container/Testimonial/Testimonial.jsx
+++ src/container/Testimonial/Testimonial.jsx
@@ -1,9 +1,19 @@
-import React, { useState, useEffect } from "react";
-import { client, urlFor } from "../../client";
+import React, { useState, useEffect, useRef } from "react";
+import { client } from "../../client";
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { images } from "../../constants";
 import "./Testimonial.scss";
+import { motion } from "framer-motion";
 
 const Testimonial = () => {
   const [testimonials, setTestimonials] = useState([]);
+  const [width, setWidth] = useState(0);
+
+  const carousel = useRef();
+
+  useEffect(() => {
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  }, []);
 
   useEffect(() => {
     const query = '*[_type =="testimonials"]';
@@ -16,7 +26,42 @@ const Testimonial = () => {
         console.log(err);
       });
   }, []);
-  return <></>;
+  return (
+    <>
+      <div className="app__testimonial">
+        <div className="app__flex app__testimonial-container">
+          <div className="app__testimonial-left">
+            <img width={278} src={images.DoubleQuotes} />
+            <h2>
+              What <br /> Customers Say
+            </h2>
+          </div>
+          <motion.div className="app__testimonial-right">
+            <motion.div className="app__testimonial-content">
+              {testimonials.map((testimonial) => (
+                <motion.div
+                  className="app__testimonial-content-item"
+                  key={testimonial}
+                >
+                  <img img width={100} src={images.DoubleQuotes} />
+                  <div>
+                    <p>"{testimonial.feedback}"</p>
+                    <p>
+                      {testimonial.name} /<span>{testimonial.company}</span>
+                    </p>
+                  </div>
+                </motion.div>
+              ))}
+            </motion.div>
+          </motion.div>
+        </div>
+      </div>
+    </>
+  );
 };
 
-export default Testimonial;
+export default AppWrap(
+  MotionWrap(Testimonial, "app__testimonial"),
+  "testimonial",
+  "app__primarybg"
+);
